cd371d10a704bb66c071dc4dff61be34fb8d3622
Digglit <Josephmarella97@gmail.com>
2020-03-12 23:36:13 -0400

    Updates

---
+++ precache-manifest.347cc2d74e54ff19369d425eb2111c90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89459ca9c6cfd10ecaf129077135815",
+    "url": "/punchclock/index.html"
+  },
+  {
+    "revision": "6b32f3bc5db88a8d9f3b",
+    "url": "/punchclock/static/css/2.6010d4dc.chunk.css"
+  },
+  {
+    "revision": "d7d2c9a7d80b6223d563",
+    "url": "/punchclock/static/css/main.8f055861.chunk.css"
+  },
+  {
+    "revision": "6b32f3bc5db88a8d9f3b",
+    "url": "/punchclock/static/js/2.e0372497.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/punchclock/static/js/2.e0372497.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d2c9a7d80b6223d563",
+    "url": "/punchclock/static/js/main.ecef507f.chunk.js"
+  },
+  {
+    "revision": "92e3337d2d85ccef393e",
+    "url": "/punchclock/static/js/runtime-main.afe49b6f.js"
+  }
+]);
